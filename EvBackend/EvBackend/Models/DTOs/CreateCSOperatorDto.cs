fbf8010de1f69b22ec6028204b589902d81e4b75
Hasindu Koshitha <hasindu.learn.online@gmail.com>
2025-09-13 10:45:28 +0530

    feat: add CreateCSOperatorDto and CreateEVOwnerDto for new user types

---
+++ EvBackend/EvBackend/Models/DTOs/CreateCSOperatorDto.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------
+// File Name: CreateCSOperatorDto.cs
+// Author: Hasindu Koshitha
+// Description: Data Transfer Object used for creating a new charging station operator
+// Created On: 13/09/2025
+// --------------------------------------------------------------
+
+namespace EvBackend.Models.DTOs
+{
+    public class CreateCSOperatorDto : CreateUserDto
+    {
+        public string StationId { get; set; }
+        public string StationName { get; set; }
+        public string StationLocation { get; set; }
+    }
+}
---
+++ EvBackend/EvBackend/Models/DTOs/CreateEVOwnerDto.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------
+// File Name: CreateEVOwnerDto.cs
+// Author: Hasindu Koshitha
+// Description: Data Transfer Object used for creating a new electric vehicle owner
+// Created On: 13/09/2025
+// --------------------------------------------------------------
+
+namespace EvBackend.Models.DTOs
+{
+    public class CreateEVOwnerDto : CreateUserDto
+    {
+        public string NIC { get; set; }
+    }
+}
