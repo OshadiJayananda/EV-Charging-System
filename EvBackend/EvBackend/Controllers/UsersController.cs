cae38ed33329c6afe9246219b4676f67a1def99e
Hasindu Koshitha <koshithahasindu@gmail.com>
2025-09-18 19:57:25 +0530

    feat: restrict access to GetAllUsers endpoint to Admin role only

--- EvBackend/EvBackend/Controllers/UsersController.cs
+++ EvBackend/EvBackend/Controllers/UsersController.cs
@@ -63,7 +63,7 @@ namespace EvBackend.Controllers
         }
 
         [HttpGet]
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetAllUsers([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
         {
             try
