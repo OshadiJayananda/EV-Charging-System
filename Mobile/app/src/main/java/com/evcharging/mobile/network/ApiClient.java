aaf4c287ddb525937d5e6f5291a1e28bd14ae0dc
Miyuri15 <miyurilokuhewage15@gmail.com>
2025-10-08 08:30:56 +0530

    added map string value

--- Mobile/app/src/main/java/com/evcharging/mobile/network/ApiClient.java
+++ Mobile/app/src/main/java/com/evcharging/mobile/network/ApiClient.java
@@ -26,7 +26,7 @@ import okhttp3.Response;
 
 public class ApiClient {
     private static final String TAG = "ApiClient";
-    private static final String BASE = "https://d5d3f12b08d7.ngrok-free.app";
+    private static final String BASE = " https://dc196aea69f4.ngrok-free.app";
     private static final String BASE_URL = BASE + "/api";
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
